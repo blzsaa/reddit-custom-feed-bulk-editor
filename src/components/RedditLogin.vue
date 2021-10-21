a90af61f96f0ad401752a76a9c56ac7fe14dbf10
blzsaa <blzsaa@users.noreply.github.com>
2021-10-21 18:44:31 +0200

    add e2e tests, added classes to Editor.vue to be more testable

--- src/components/RedditLogin.vue
+++ src/components/RedditLogin.vue
@@ -4,30 +4,42 @@
       To use this page you first have to allow reddit-custom-feed-bulk-editor to
       access which subreddits and multireddits you are subscriber to.
     </p>
-    <Button
-      class="p-button-rounded"
-      @click="redirectToRedditAuthenticationPage"
-    >
-      Login with reddit account
-    </Button>
+    <a :href="linkToRedditAuthenticationPage()">
+      <Chip
+        class="p-mr-2 p-mb-2 custom-chip"
+        label="Login with reddit account"
+        image="https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png"
+      />
+    </a>
   </div>
 </template>
 
 <script setup lang="ts">
-import Button from "primevue/button/sfc";
+import Chip from "primevue/chip/sfc";
 
-function redirectToRedditAuthenticationPage() {
+function linkToRedditAuthenticationPage() {
   const redirectUri = "http://localhost:8080/authorize_callback";
   const responseType = "token";
   const state = "STATE";
   const scopes = ["mysubreddits", "read", "subscribe"];
   // send the user to the authentication url
-  window.location.href =
+  return (
     "https://www.reddit.com/api/v1/authorize?" +
     `client_id=${process.env.VUE_APP_CLIENT_ID}` +
     `&response_type=${responseType}` +
     `&state=${state}` +
     `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-    `&scope=${encodeURIComponent(scopes.join(" "))}`;
+    `&scope=${encodeURIComponent(scopes.join(" "))}`
+  );
 }
 </script>
+
+<style scoped>
+.custom-chip {
+  background: var(--primary-color);
+  color: var(--primary-color-text);
+}
+a {
+  text-decoration: none;
+}
+</style>
--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add(
+  "shouldBeChecked",
+  { prevSubject: "element" },
+  (subject) => {
+    return cy.get(subject).should("have.class", "p-checkbox-checked");
+  }
+);
+
+Cypress.Commands.add(
+  "shouldNotBeChecked",
+  { prevSubject: "element" },
+  (subject) => {
+    return cy.get(subject).should("not.have.class", "p-checkbox-checked");
+  }
+);
