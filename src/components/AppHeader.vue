1bb4bcaa9e60e86b90bee2f528c75e84821ed48a
blzsaa <blzsaa@users.noreply.github.com>
2024-01-30 15:49:36 +0100

    refactor: 💡 refactor header to toolbar from data-table header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ChangeSaver from "@/components/ChangeSaver.vue";
+</script>
+
+<template>
+  <div class="card">
+    <Toolbar>
+      <template #start>
+        <b>Reddit custom feed bulk editor</b>
+      </template>
+      <template #end>
+        <change-saver />
+      </template>
+    </Toolbar>
+  </div>
+</template>
